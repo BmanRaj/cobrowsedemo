364b7c1e8d81af5dd7f577ebe172648fc898884d
Burhan Rajbhoy <32659564+BmanRaj@users.noreply.github.com>
2025-08-27 15:34:24 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Listen for the ZoomZccCobrowseSDK:Ready event
+window.addEventListener('ZoomZccCobrowseSDK:Ready', function(event) {
+    const button = document.getElementById('startCobrowseButton'); // Your custom button ID
+
+    if (button) {
+        // Optional: Enable the button if it is disabled
+        button.disabled = false;
+        
+        // Add a click event to start the Cobrowse session
+        button.addEventListener('click', function() {
+            try {
+                window.ZoomZccCobrowseSDK.init();
+            } catch (e) {
+                console.log("ZoomZccCobrowseSDK is not ready, please try again later")
+            }
+        });
+    }
+}); 
